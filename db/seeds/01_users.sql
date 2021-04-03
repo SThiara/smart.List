13963b78e71c6369fbec35dc219da8db5321f1ab
Sasha Mahalia <sasha.camila.mahalia@gmail.com>
2021-04-03 21:03:05 +0000

    Creates 01_users and 02_list_items tables and fills with seed data

--- db/seeds/01_users.sql
+++ db/seeds/01_users.sql
@@ -1,3 +1,7 @@
--- Users table seeds here (Example)
-INSERT INTO users (name) VALUES ('Alice');
-INSERT INTO users (name) VALUES ('Kira');
+-- Users table seeds here
+INSERT INTO users (name, email, password) VALUES ('Alice Jones', 'alicejones@gmail.com', '$2a$10$FB/BOAVhpuLvpOREQVmvmezD4ED/.JBIDRh70tGevYzYzQgFId2u.');
+INSERT INTO users (name, email, password) VALUES ('Linus Vibiana', 'linusvibiana@gmail.com', '$2a$10$FB/BOAVhpuLvpOREQVmvmezD4ED/.JBIDRh70tGevYzYzQgFId2u.');
+INSERT INTO users (name, email, password) VALUES ('Katerina Hero', 'katerinahero@gmail.com', '$2a$10$FB/BOAVhpuLvpOREQVmvmezD4ED/.JBIDRh70tGevYzYzQgFId2u.');
+INSERT INTO users (name, email, password) VALUES ('Hieremihel Pythagoras', 'pythagoras@gmail.com', '$2a$10$FB/BOAVhpuLvpOREQVmvmezD4ED/.JBIDRh70tGevYzYzQgFId2u.');
+INSERT INTO users (name, email, password) VALUES ('Mahdi Dzidra', 'mahdidzidras@gmail.com', '$2a$10$FB/BOAVhpuLvpOREQVmvmezD4ED/.JBIDRh70tGevYzYzQgFId2u.');
+
