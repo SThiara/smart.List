3cb20512fa64188ae3547d9987fed33b974185d5
mwkho <martin.h@hotmail.ca>
2021-04-05 23:21:29 +0000

    Make ajax call upon submission of todo list

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,10 +1,37 @@
+const escape =  function(str) {
+  let div = document.createElement('div');
+  div.appendChild(document.createTextNode(str));
+  return div.innerHTML;
+};
+
+const createToDoItem = (todo) => {
+  return `<li class=list-group-item>${escape(todo.contents)}</li>`;
+};
+
+const renderTodos = (todos) => {
+  console.log('todos: ', todos);
+};
+
+
 $(() => {
-  $.ajax({
-    method: "GET",
-    url: "/api/users"
-  }).done((users) => {
-    for(user of users) {
-      $("<div>").text(user.name).appendTo($("body"));
-    }
-  });;
+  $('form').on("submit", function(event){
+    event.preventDefault();
+    $.ajax({
+      method:"POST",
+      url:"/lists/",
+      success: () => {
+        console.log('posting into db success')
+        $.ajax({
+          method:"GET",
+          url:"/lists/",
+          // getting an array of all the list then rendering them out
+          success: (lists) => {
+            for(let list of lists){
+              console.log(list)
+            }
+          }
+        });
+      }
+    });
+  })
 });
