15c079059555c3a109df5e4ad383a53e62f2306f
Sasha Mahalia <sasha.camila.mahalia@gmail.com>
2021-04-06 16:49:45 +0000

    Comments out non-exisitant user-routes address

--- server.js
+++ server.js
@@ -33,12 +33,12 @@ app.use(cookieSession({
 // Separated Routes for each Resource
 // Note: Feel free to replace the example routes below with your own
 
-const usersRoutes = require("./routes/user-routes");
+// const usersRoutes = require("./routes/user-routes");
 const listsRoutes = require("./routes/list-routes");
 
 // Mount all resource routes
 // Note: Feel free to replace the example routes below with your own
-app.use("/user", usersRoutes());
+// app.use("/user", usersRoutes());
 app.use("/lists", listsRoutes());
 
 // Note: mount other resources here, using the same pattern above
