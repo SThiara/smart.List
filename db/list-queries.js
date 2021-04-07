c23d8beb26f386d480960fbba99ec468fd5a0547
mwkho <martin.h@hotmail.ca>
2021-04-07 23:02:58 +0000

    Fix queries to have id tag in each to-do

--- db/list-queries.js
+++ db/list-queries.js
@@ -1,7 +1,7 @@
 const db = require('./dbsetup');
 
 const listByCategory = (category, id) => {
-  let query = `SELECT name, category FROM list_items
+  let query = `SELECT name, category, id FROM list_items
     WHERE category = $1
     AND user_id = $2
     ORDER BY name`;
